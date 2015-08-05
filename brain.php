610f882dbedd515ec2db6b574918c6c97d549e61
System Administrator <root@192.168.1.12>
2015-08-04 22:54:59 -0400

    misc

---
+++ brain.php
@@ -0,0 +1,78 @@
+<?php
+
+/* Check all form inputs using check_input function */
+// $yourcity = check_input($_POST['yourcity'], "Choose a city");
+$yourcity = $_POST['city'];
+printf($yourcity);
+
+
+
+
+// /* Functions we used */
+// function check_input($data, $problem='')
+// {
+//     $data = trim($data);
+//     $data = stripslashes($data);
+//     $data = htmlspecialchars($data);
+//     if ($problem && strlen($data) == 0)
+//     {
+//         show_error($problem);
+//     }
+//     return $data;
+// }
+
+// call Yahoo weather Nome AK
+// function check_one_city($yourcity)
+// {
+// $access_token = 'token'; // access_token received from /oauth2/token call
+// $ch = curl_init(
+// 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' . $yourcity . '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+// ); // URL of the call
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER,
+//     array("Authorization: Bearer $access_token"));
+// // execute the api call
+// $result = curl_exec($ch);
+// // decode json
+// $decoded = (json_decode($result, true));
+// //close curl
+// curl_close($ch);
+// //return result
+// return ($decoded);
+// // }
+
+
+        // create curl handle 
+        $ch = curl_init(); 
+
+        // set url, with $yourcity concatenated into the url.
+        curl_setopt($ch, CURLOPT_URL, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" . $yourcity . "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"); 
+
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+        // execute the curl and put the result into var $output
+        $output = curl_exec($ch); 
+
+        // close curl 
+        curl_close($ch); 
+
+        $decoded = json_decode($output, true);
+
+        echo $output;   
+        // print($decoded);
+
+// access first element of $ar array
+echo $output[0];
+
+
+// //Iterate through the decoded json
+// foreach($decoded as $key => $value)
+// {
+//    echo $value['period'] . '<br>';
+//    echo $value['icon'] . '<br>';
+// }
+        
+
+
+?>
---
+++ form.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+
+
+<form action="form.php" method="post">
+
+<p>Get a weather report for your city:
+
+<select name="city">
+<option value=""> -- Please select -- </option>
+<option value="Baltimore">Baltimore, MD</option>
+<option value="Seattle">Seattle, WA</option>
+<option value="LosAngeles">Los Angeles, CA</option>
+<option value="Nome">Nome, AK</option>
+<option value="Paris">Paris, France</option>
+</select>
+
+<p><input type="submit" value="Check Weather!"></p>
+
+</form>
+
+Your weather report: <br />
+<?php
+include 'brain.php';
+?>
+ 
+
+</body>
+</html>
+
+
