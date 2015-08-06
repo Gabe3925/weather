553c5647c06d1297010bb1e15042f994f8ecab22
Gabe <gsnydermail@gmail.com>
2015-08-06 19:37:07 -0400

    zip function tweak

--- brain.php
+++ brain.php
@@ -9,7 +9,7 @@ function return_weather_for_zip($zip)
           $ch = curl_init(); 
 
           // set url, with $yourcity concatenated into the url.
-          curl_setopt($ch, CURLOPT_URL, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" . $yourcity . "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"); 
+          curl_setopt($ch, CURLOPT_URL, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" . $zip . "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"); 
 
           //return the transfer as a string 
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
